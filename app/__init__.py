fd126fc63948ec37f9f369729b565c2424946eb9
buddha314 <buddha_314@yahoo.com>
2017-09-16 16:50:56 -0700

    now moving and eating can be fun in this world

--- app/__init__.py
+++ app/__init__.py
@@ -9,9 +9,9 @@ from models import *
 n_lat = 100
 n_long = 100
 n_initial_inhabitants = 20
-n_days= 365 
+n_days= 365
 n_years = 2
 
 
 world = World(n_lat,n_long, n_initial_inhabitants)
-run_world(world, n_days)
+#run_world(world, n_days)
--- app/models.py
+++ app/models.py
@@ -21,8 +21,8 @@ stat_bonus[18] = 4
 
 
 class Automata:
-    current_x = 0
-    current_y = 0
+    current_lon = 0
+    current_lat = 0
 
     def __init__(self):
         # Stats
@@ -40,6 +40,7 @@ class Automata:
 
         # Health and wellness
         self.hit_points = random.randint(7,12) + stat_bonus[self.con]
+        self.current_hit_points = self.hit_points
 
     def decide_to_travel(self):
         r = math.tanh(self.bravado - self.aggressiveness)
@@ -48,16 +49,28 @@ class Automata:
         else:
             return False
 
+    def move(self):
+        if self.decide_to_travel():
+            self.current_lon = self.current_lon + random.randint(-1,1)
+            self.current_lat = self.current_lat + random.randint(-1,1)
+            print("moving to ({lat}, {lon})".format(lat=self.current_lat, lon=self.current_lon))
+
     def age(self):
-        self.hit_points += -1
-        return self.hit_points
+        self.current_hit_points += -1
+        return self.current_hit_points
 
 class Cell:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        #self.fertility = random.random()
         self.fertility = random.random()
-        self.food_units = random.randint(1,10)
+        self.food_units = random.randint(0,10)
+
+    def grow(self):
+        if random.random() < self.fertility:
+            self.food_units += 1
+
 
 class World:
     def __init__(self, n_lat, n_lon, n_pop):
@@ -66,14 +79,14 @@ class World:
         self.n_lon = n_lon
         self.cells = np.empty((n_lat, n_lon), dtype=object)
         self.population = []
-        for i in range(n_lat):
-            for j in range(n_lon):
+        for i in range(n_lon):
+            for j in range(n_lat):
                 self.cells[i,j] = Cell(i,j)
 
         for i in range(n_pop):
             a = Automata()
-            a.current_x = random.randint(0,self.n_lat)
-            a.current_y = random.randint(0,self.n_lon)
+            a.current_lon = random.randint(0,self.n_lat-1)
+            a.current_lat = random.randint(0,self.n_lon-1)
             self.population.append(a)
 
     def one_day(self):
@@ -82,6 +95,11 @@ class World:
             h = p.age()
             if h <= 0:
                 self.population.remove(p)
+            else:
+                a_cell = self.cells[p.current_lon, p.current_lat]
+                self.consider_move(p, a_cell)
+                self.farm_cell(p, a_cell)
+
         self.day_report()
         if len(self.population) <= 0:
             print("Day {day} APOCALPYSE!".format(day=self.day))
@@ -89,6 +107,16 @@ class World:
         else:
             return True
 
+    def consider_move(self, automata, cell):
+        automata.move()
+        automata.current_lon = max(min(automata.current_lon, self.n_lon-1),0)
+        automata.current_lat = max(min(automata.current_lat, self.n_lat-1),0)
+
+    def farm_cell(self, automata, cell):
+        fu = min(automata.hit_points - automata.current_hit_points, cell.food_units)
+        cell.food_units -= fu
+        #print("cell units left: {u}".format(u=cell.food_units))
+        automata.current_hit_points = automata.current_hit_points + fu
 
     def day_report(self):
         print("Day: {day}\n\tliving population: {pop_size}\n".format(day=self.day, pop_size=len(self.population)))
